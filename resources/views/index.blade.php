c219451bf03a197d1bde6f0d41c1dfd310fd11e4
kirknacor <78600487+kirknacor@users.noreply.github.com>
2022-03-30 02:09:49 +0800

    Create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="card">
+  <div class="card-header">
+    Index Page
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+@endsection
