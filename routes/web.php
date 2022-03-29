6cb3ccea026823b6f7ab724038e7a44d055d8b38
kirknacor <78600487+kirknacor@users.noreply.github.com>
2022-03-30 02:10:12 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\PageController::class, 'index']);
+Route::resource('/post', App\Http\Controllers\PostController::class);
+Route::get('/about', [App\Http\Controllers\PageController::class, 'about']);
+Route::get('/profile', [App\Http\Controllers\PageController::class, 'profile']);
